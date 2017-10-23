ebe88d0953884d89cffb13125e310f8c93845273
xiedan <q1w2e3r$>
2017-10-23 17:00:15 +0800

    task status

--- examples/routers/taskStatus.vue
+++ examples/routers/taskStatus.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="width: 400px;margin: 200px;">
         <TaskStatus name="1">
-            史蒂夫·乔布斯
+            <p slot="title">史蒂夫·乔布斯（Steve Jobs）</p>
             <p slot="content">史蒂夫·乔布斯（Steve Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。</p>
         </TaskStatus>
     </div>
